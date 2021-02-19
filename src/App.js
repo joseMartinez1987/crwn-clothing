d80c0184d59e2514041188e3260d193a6801464e
Jose Martinez <jmartinez@ecomsur.com>
2021-02-19 16:47:25 -0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import './App.css';
+import HomePage from './pages/homePage/HomePage'
+import {Switch, Route} from 'react-router-dom'
+import ShopPage from './pages/shop/shop'
+import Header from './components/header/header'
+import SignInAndSignUp from './pages/sign-in-and-sign-up/sign-in-and-sign-up'
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <Switch>
+        <Route exact path='/' component={HomePage} />
+        <Route  path='/shop' component={ShopPage} / >
+        <Route  path='/signin' component={SignInAndSignUp} / >
+      </Switch>
+
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/preview-collection/preview-collection.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import './preview-collection.scss'
+
+import CollectionItem from './../collection-item/collection-item'
+
+const PreviewCollection = ({title, items}) => (
+
+    <div className='collection-preview'>
+        <h1 className='title'>{title.toUpperCase()}</h1>
+        <div className="preview">
+            {
+                items.filter((item,index) => index < 4).map(({id, ...othersProps}) => (
+                    <CollectionItem key={id} {...othersProps} / >
+                ))
+            }
+        </div>
+    </div>
+)
+
+export default PreviewCollection
+
