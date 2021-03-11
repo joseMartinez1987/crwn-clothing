e1b264da95a3ef9756bab08caaf8a771c81c5ece
Jose Martinez <jmartinez@ecomsur.com>
2021-03-11 10:57:44 -0300

    updating redirect when logging

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import './App.css';
 import HomePage from './pages/homePage/HomePage'
-import {Switch, Route} from 'react-router-dom'
+import {Switch, Route, Redirect} from 'react-router-dom'
 import ShopPage from './pages/shop/shop'
 import Header from './components/header/header'
 import SignInAndSignUp from './pages/sign-in-and-sign-up/sign-in-and-sign-up'
@@ -45,7 +45,7 @@ class App extends React.Component {
         <Switch>
           <Route exact path='/' component={HomePage} />
           <Route  path='/shop' component={ShopPage} / >
-          <Route  path='/signin' component={SignInAndSignUp} / >
+          <Route exact  path='/signin' render ={() => this.props.currentUser ? (<Redirect to='/'/>) : (<SignInAndSignUp />)} / >
         </Switch>
   
       </div>
@@ -53,8 +53,12 @@ class App extends React.Component {
   }
 }
 
+const mapStateToProps = ({user}) => ({
+  currentUser: user.currentUser
+}) 
+
 const mapDispatchToProps = dispatch => ({
   setCurrentUser: user => dispatch(setCurrentUser(user))
 })
 
-export default connect(null,mapDispatchToProps)(App);
+export default connect(mapStateToProps,mapDispatchToProps)(App);
